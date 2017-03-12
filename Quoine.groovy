7653cc942ec9e527d727ecf9f029d7346d4a2a3a
wuqianming <379054393@qq.com>
2017-03-12 23:11:55 +0800

    Add files via upload

--- Quoine.groovy
+++ Quoine.groovy
@@ -1,6 +1,30 @@
+import org.knowm.xchange.Exchange
+import org.knowm.xchange.currency.CurrencyPair
+import org.knowm.xchange.quoine.QuoineExchange
+import org.knowm.xchange.ExchangeSpecification
+import org.knowm.xchange.quoine.service.QuoineAccountService
+import org.knowm.xchange.quoine.service.QuoineMarketDataService
 
-/**
- * Created by wuqianming on 2017/3/12.
- */
-class Quoine {
+
+
+def createxchange(){
+    def exchange = new QuoineExchange()
+    def exchangeSpec = new ExchangeSpecification(exchange.class)
+
+    exchangeSpec.getExchangeSpecificParameters().put(QuoineExchange.KEY_TOKEN_ID,"69817")
+    exchangeSpec.getExchangeSpecificParameters().put(QuoineExchange.KEY_USER_SECRET,"63Tm0R+hMVeJ9d/luVGWOJ8nXei5mv9YrvQlsVASEehL58N+hzWrzEoosyQmpjYY2eYvZVK4jcHhxwmmUSPUCQ==")
+    exchange.applySpecification(exchangeSpec)
+    return exchange
+}
+
+
+quoine = createxchange()
+
+//market = quoine.getAccountService()
+while (1) {
+
+
+    println(quoine.getMarketDataService().getTicker(CurrencyPair.BTC_JPY))
+    sleep(500)
+
 }
